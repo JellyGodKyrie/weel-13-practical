2e000049a667e9a566078fd6f6fff66d832d4f5a
Alioune Seck <aliouneseck15@gmail.com>
2023-12-25 17:50:57 -0500

    Tip calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -6,6 +6,12 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hello, ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Total: $${req.params.total}, Tip Percentage: ${req.params.tipPercentage}%, Tip Amount: $${req.params.total * (req.params.tipPercentage / 100)}`)
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`The PORT at ${PORT} is open`)
 })
