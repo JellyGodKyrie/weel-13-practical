2e7cb8ce5fca17a90679b4f1f4e3710b6d292cd8
Alioune Seck <aliouneseck15@gmail.com>
2023-12-25 17:39:49 -0500

    Greeting express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3001
 
-app.listen(3001, () => {
-    console.log('Hello World')
+app.get('/greeting/:name', (req, res) => {
+    res.send(`Hello, ${req.params.name}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`The PORT at ${PORT} is open`)
 })
